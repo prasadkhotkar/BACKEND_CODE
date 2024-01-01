7d11efc0dd0f8d556901296e74a9628c8565830e
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2024-01-01 20:36:52 +0530

    model ,routes code added

--- AuthApp/index.js
+++ AuthApp/index.js
@@ -6,3 +6,14 @@ require("dotenv").config();
 const PORT=process.env.PORT ||4000;
 
 app.use(express.json());
+
+require("./config/database").connect();
+
+//import route and mount it
+const user=require("./routes/user");
+
+//server activation
+
+app.listen(PORT,()=>{
+    console.log(`App is listening on ${PORT}`);
+})
