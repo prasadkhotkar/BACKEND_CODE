e05a1c8d26741b93631b551d7cfb7e5738674780
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2024-01-02 09:40:49 +0530

    signup code for controller added

--- AuthApp/routes/user.js
+++ AuthApp/routes/user.js
@@ -1,9 +1,9 @@
-const express =require("express");
+const express=require("express")
 const router=express.Router();
 
-const {login,signup}=require("../controllers/Auth");
+const {login ,signup}=require("../controllers/Auth");
 
-router.post("/login",login);
-router.post("/signup",signup);
+//router.post("/login" ,login);
+router.post("/signup", signup);
 
 module.exports=router;
